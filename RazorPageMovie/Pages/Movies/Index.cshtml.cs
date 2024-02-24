6752fbb10a38b97436ce7404465f90b466270f1e
Cristina Aguilar Rodriguez <aguilar.c@ugto.mx>
2024-02-24 13:20:52 -0600

    Agregar archivos de proyecto.

---
+++ RazorPageMovie/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPageMovie.Data;
+using RazorPageMovie.Models;
+
+namespace RazorPageMovie.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPageMovie.Data.RazorPageMovieContext _context;
+
+        public IndexModel(RazorPageMovie.Data.RazorPageMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+        public SelectList? Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
+        public async Task OnGetAsync()
+        {
+            //Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+        var movies = from m in _context.Movie
+                     select m;
+
+            if(!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(x => x.Title.Contains(SearchString));
+
+            }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+
+            if (_context.Movie != null)
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+                Movie = await movies.ToListAsync();
+            }
+        }
+    }
+}
---
+++ RazorPageMovie/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPageMovie.Data;
+using RazorPageMovie.Models;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPageMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPageMovieContext") ?? throw new InvalidOperationException("Connection string 'RazorPageMovieContext' not found.")));
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
